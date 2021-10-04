2bcbae6470286b949a716c85604c6d0626bcfc76
Vorobiov Vladyslav <vorobyov.vladislav@gmail.com>
2021-10-04 21:31:05 +0300

    add useEffect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import {useEffect, useState} from 'react'
 import Input from './components/Input'
 
 function App() {
+  const [counter, setCounter] = useState(0)
+  const [anotherCounter, setAnotherCounter] = useState(0)
   console.log('render component');
- 
+  
+  useEffect(() => {
+    console.log('render component in the use effect when anotherCounter changed')
+  }, [counter])
+  
+  
   return (
     <>
       
       <Input  name="name" label="Login"/>
       <Input  name="password" type="password" label="Password"/>
-      
+      <div onClick={() => setCounter(counter + 1)}>Counter: {counter}</div>
+      <div onClick={() => setAnotherCounter(anotherCounter + 1)}>Another Counter: {anotherCounter}</div>
       
     </>
   );
